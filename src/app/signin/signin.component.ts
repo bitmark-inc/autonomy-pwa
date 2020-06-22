ecd263eb0cd48fbea6a14ac54041698457d7c034
Cuong Le <cuongle@bitmark.com>
2020-06-22 12:09:16 +0700

    Add sub components for Signin and Signup page

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+enum EnumPageStage { RecoveryPhrase, Permission }
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+
+  public PageStage = EnumPageStage;
+  public stage: EnumPageStage = EnumPageStage.RecoveryPhrase;
 
   constructor() { }
 
