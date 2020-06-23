a848de454ea2fa7718810cab6d67828a35e6add7
duyenbui <duyen@bitmark.com>
2020-06-23 09:01:49 +0700

    sign up onboarding

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 enum EnumPageStage { Intro01, Intro02, Intro03, Permission }
 
@@ -11,9 +12,38 @@ export class SignupComponent implements OnInit {
 
   public PageStage = EnumPageStage;
   public stage: EnumPageStage = EnumPageStage.Intro01;
+  public locationChecked: boolean;
+  public notifyChecked: boolean;
+  public notifyState: any;
+  public locationState: any;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.locationChecked = false;
+    this.notifyChecked = false;
+    this.notifyState = '/assets/img/plus.svg'
+    this.locationState = '/assets/img/plus.svg'
+  }
 
   ngOnInit() {}
+
+  public locationPermission() {
+    this.locationChecked = true;
+    if (this.locationChecked) {
+      this.locationState = '/assets/img/checked.svg';
+    }
+  }
+
+  public notifyPermission() {
+    this.notifyChecked = true;
+    if (this.notifyChecked) {
+      this.notifyState = '/assets/img/checked.svg';
+    }
+  }
+
+  public done() {
+    if (this.locationChecked && this.notifyChecked) {
+      this.router.navigate(['dashboard']);
+    }
+  }
 
 }
