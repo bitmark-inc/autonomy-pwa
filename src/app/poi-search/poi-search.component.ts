a67098b1f8e0299b7eb1255d291ea264b40bf414
duyenbui <duyen@bitmark.com>
2020-06-27 17:10:27 +0700

    search poi by tag

--- src/app/poi-search/poi-search.component.ts
+++ src/app/poi-search/poi-search.component.ts
@@ -13,30 +13,33 @@ import { ApiService } from '../services/api/api.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-poi-search',
-  templateUrl: './poi-search.component.html',
-  styleUrls: ['./poi-search.component.scss']
+  selector: "app-poi-search",
+  templateUrl: "./poi-search.component.html",
+  styleUrls: ["./poi-search.component.scss"],
 })
 export class PoiSearchComponent implements OnInit {
+  @ViewChild("placeSearchInput", { static: true }) placeSearchInput: ElementRef;
+  @ViewChild("googleMapAttribution", { static: true })
+  googleMapAttribution: ElementRef;
 
-  @ViewChild('placeSearchInput', { static: true }) placeSearchInput: ElementRef;
-  @ViewChild('googleMapAttribution', { static: true }) googleMapAttribution: ElementRef;
-
-  public keyword: string = '';
+  public keyword: string = "";
   public placesByKeyword: {
-    name: string,
-    formatted_address: string,
-    place_id: string
+    name: string;
+    formatted_address: string;
+    place_id: string;
   }[];
 
   public resources: {
-    id: string,
-    name: string,
+    id: string;
+    name: string;
   }[];
   public resourceID: string;
   public poisByResource: {
-    id: string,
-    alias: string,
+    id: string;
+    alias: string;
+    address: string;
+    resource_score: number;
+    distance: number;
   }[];
 
   private placeService: any;
@@ -44,43 +47,60 @@ export class PoiSearchComponent implements OnInit {
   constructor(private apiService: ApiService, private ref: ChangeDetectorRef, public router: Router, private ngZone: NgZone) {}
 
   ngOnInit() {
-    fromEvent(this.placeSearchInput.nativeElement, 'keyup').pipe(
-      map((event: any) => {
-        return this.keyword;
-      }),
-      filter(res => res.length > 2),
-      debounceTime(500),
-      distinctUntilChanged()
-    ).subscribe(() => {
-      this.searchByKeyword();
-    });
+    fromEvent(this.placeSearchInput.nativeElement, "keyup")
+      .pipe(
+        map((event: any) => {
+          return this.keyword;
+        }),
+        filter((res) => res.length > 2),
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe(() => {
+        this.searchByKeyword();
+      });
 
     this.getResourcesForSearching();
   }
 
   private initPlaceService() {
     if (!this.placeService) {
-      this.placeService = new window.google.maps.places.PlacesService(this.googleMapAttribution.nativeElement);
+      this.placeService = new window.google.maps.places.PlacesService(
+        this.googleMapAttribution.nativeElement
+      );
     }
     return this.placeService;
   }
 
-  private searchByKeyword(){
+  private searchByKeyword() {
     this.initPlaceService();
-    this.placeService.textSearch({query: this.keyword}, (result, status) => {
+    this.placeService.textSearch({ query: this.keyword }, (result, status) => {
       if (status === window.google.maps.places.PlacesServiceStatus.OK) {
         this.placesByKeyword = result;
         this.ref.detectChanges();
       } else {
-        console.log('Error searching places from google map service');
+        console.log("Error searching places from google map service");
         // TODO: do something
       }
-    })
+    });
   }
 
-  private searchByResource(){
+  private getResourcesForSearching() {
+    this.apiService.request("get", `api/resources?suggestion=true`).subscribe(
+      (data: { resources: any }) => {
+        this.resources = data.resources;
+      },
+      (err) => {
+        console.log(err);
+        // TODO: do something
+      }
+    );
+  }
+
+  public searchByResource(resource) {
+    this.resourceID = resource.name;
     this.apiService
-      .request('get', `api/points-of-interest?resource_id=${this.resourceID}`)
+      .request("get", `api/points-of-interest?resource_id=${resource.id}`)
       .subscribe(
         (data) => {
           this.poisByResource = data;
@@ -92,32 +112,23 @@ export class PoiSearchComponent implements OnInit {
       );
   }
 
-  private getResourcesForSearching() {
-    this.apiService
-      .request('get', `api/resources?suggestion=true`)
-      .subscribe(
-        (data: {resources: any}) => {
-          this.resources = data.resources;
-        },
-        (err) => {
-          console.log(err);
-          // TODO: do something
-        }
-      );
+  public clearAll() {
+    this.keyword = "";
+    this.resourceID = "";
   }
 
   public navigateToPlace(place: any) {
     this.apiService
-      .request('post', 'api/points-of-interest', {
+      .request("post", "api/points-of-interest", {
         alias: place.name,
         address: place.formatted_address,
         location: {
           latitude: place.geometry.location.lat(),
-          longitude: place.geometry.location.lng()
-        }
+          longitude: place.geometry.location.lng(),
+        },
       })
       .subscribe(
-        (data: {id: string}) => {
+        (data: { id: string }) => {
           this.ngZone.run(() => {
             this.navigateToPOI(data.id);
           });
@@ -130,7 +141,6 @@ export class PoiSearchComponent implements OnInit {
   }
 
   public navigateToPOI(id: string) {
-    this.router.navigate(['/pois', id]);
+    this.router.navigate(["/pois", id]);
   }
-  
 }
