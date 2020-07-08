40ac88ad820b2964aaf5f84f13c94fe835a0a08b
duyenbui <duyen@bitmark.com>
2020-07-08 13:51:46 +0700

    demo resources tab UI

--- src/app/home/resources/resources.component.ts
+++ src/app/home/resources/resources.component.ts
@@ -1,15 +1,32 @@
+import { ApiService } from './../../services/api/api.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-resources',
-  templateUrl: './resources.component.html',
-  styleUrls: ['./resources.component.scss']
+  selector: "app-resources",
+  templateUrl: "./resources.component.html",
+  styleUrls: ["./resources.component.scss"],
 })
 export class ResourcesComponent implements OnInit {
+  public resources: {
+    id: string;
+    name: string;
+  }[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private apiService: ApiService) {
+    this.getResourcesForSearching();
   }
 
+  ngOnInit() {}
+
+  private getResourcesForSearching() {
+    this.apiService.request("get", `api/resources?suggestion=true`).subscribe(
+      (data: { resources: any }) => {
+        this.resources = data.resources;
+      },
+      (err) => {
+        console.log(err);
+        // TODO: do something
+      }
+    );
+  }
 }
