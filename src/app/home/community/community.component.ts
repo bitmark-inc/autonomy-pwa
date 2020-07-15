17352c3c268bb111f4a130cfdd12fbd6933455ff
Cuong Le <cuongleqq@gmail.com>
2020-07-15 11:15:24 +0700

    Get real data for community component

--- src/app/home/community/community.component.ts
+++ src/app/home/community/community.component.ts
@@ -29,142 +29,142 @@ export class CommunityComponent implements OnInit {
 
   constructor(private apiService: ApiService, private renderer: Renderer) {
     this.checkedInPerson = 1234;
-    this.dataBySymptoms = [
-      {
-        id: "fever",
-        name: "Fever",
-        value: 5,
-        change_rate: -82.14285714285714,
-        distribution: {},
-        checked: false,
-      },
-      {
-        id: "throat",
-        name: "Sore throat",
-        value: 3,
-        change_rate: -84.21052631578947,
-        distribution: {},
-        checked: false,
-      },
-      {
-        id: "cough",
-        name: "Cough",
-        value: 3,
-        change_rate: -86.36363636363636,
-        distribution: {},
-        checked: false,
-      },
-      {
-        id: "muscle_pain",
-        name: "Muscle pain",
-        value: 2,
-        change_rate: 100,
-        distribution: {},
-        checked: false,
-      },
-      {
-        id: "suggestion_101",
-        name: "Sleeplessness (insomnia)",
-        value: 1,
-        change_rate: -50,
-        distribution: {},
-        checked: false,
-      },
-      {
-        id: "loss_taste_smell",
-        name: "New loss of taste or smell",
-        value: 1,
-        change_rate: 100,
-        distribution: {},
-        checked: false,
-      },
-      {
-        id: "suggestion_58",
-        name: "Heartburn (pyrosis)",
-        value: 0,
-        change_rate: -100,
-        distribution: {},
-        checked: false,
-      },
-      {
-        id: "e46a3314e9466dec61e2bbca4cf8b38bc8dbd58ca031b3a702ef3a43b82deb02",
-        name: "Test",
-        value: 0,
-        change_rate: -100,
-        distribution: {},
-        checked: false,
-      },
-      {
-        id: "5cfae3932e8ef619a14bca9f49cb1348d0be7ae17aff77117b8fcb3067191a31",
-        name: "吃飽太閒",
-        value: 0,
-        change_rate: -100,
-        distribution: {},
-        checked: false,
-      },
-      {
-        id: "19155e1e1873ef94470a7c82d83a7fcc56cd904c73234791a5fcaf720f2a79a6",
-        name: "Need coffee",
-        value: 0,
-        change_rate: -100,
-        distribution: {},
-        checked: false,
-      },{
-        id: "c29b18bae2991272d820eae4c3f8061bfb5495a2f5a119586f3785b2711d5055",
-        name: "Sleepy",
-        value: 0,
-        change_rate: -100,
-        distribution: {}
-      }, {
-        id: "face",
-        name: "Bluish lips or face",
-        value: 0,
-        change_rate: -100,
-        distribution: {}
-      }, {
-        id: "nasal",
-        name: "Nasal congestion",
-        value: 0,
-        change_rate: -100,
-        distribution: {}
-      }, {
-        id: "2e4a33f963deb6f28ff475891b3441b4b1582479423c655a7e36ce2350f1d442",
-        name: "Tired too much",
-        value: 0,
-        change_rate: -100,
-        distribution: {}
-      }, {
-        id: "a8db5c5202904141d3c86d9ba8e2a9950e49573d5498539829128c9beca32aa1",
-        name: "Loss of sense of smell",
-        value: 0,
-        change_rate: -100,
-        distribution: {}
-      }, {
-        id: "breath",
-        name: "Shortness of breath or difficulty breathing",
-        value: 0,
-        change_rate: -100,
-        distribution: {}
-      }, {
-        id: "fatigue",
-        name: "Fatigue or tiredness",
-        value: 0,
-        change_rate: -100,
-        distribution: {}
-      }, {
-        id: "690d323f97b4f7849c3c3f551b7f9912d99ebde5e77666de1911d56b9c5da440",
-        name: "火眼金星",
-        value: 0,
-        change_rate: -100,
-        distribution: {}
-      }, {
-        id: "chest",
-        name: "Chest pain",
-        value: 0,
-        change_rate: -100,
-        distribution: {}
-      }
-    ];
+    // this.dataBySymptoms = [
+    //   {
+    //     id: "fever",
+    //     name: "Fever",
+    //     value: 5,
+    //     change_rate: -82.14285714285714,
+    //     distribution: {},
+    //     checked: false,
+    //   },
+    //   {
+    //     id: "throat",
+    //     name: "Sore throat",
+    //     value: 3,
+    //     change_rate: -84.21052631578947,
+    //     distribution: {},
+    //     checked: false,
+    //   },
+    //   {
+    //     id: "cough",
+    //     name: "Cough",
+    //     value: 3,
+    //     change_rate: -86.36363636363636,
+    //     distribution: {},
+    //     checked: false,
+    //   },
+    //   {
+    //     id: "muscle_pain",
+    //     name: "Muscle pain",
+    //     value: 2,
+    //     change_rate: 100,
+    //     distribution: {},
+    //     checked: false,
+    //   },
+    //   {
+    //     id: "suggestion_101",
+    //     name: "Sleeplessness (insomnia)",
+    //     value: 1,
+    //     change_rate: -50,
+    //     distribution: {},
+    //     checked: false,
+    //   },
+    //   {
+    //     id: "loss_taste_smell",
+    //     name: "New loss of taste or smell",
+    //     value: 1,
+    //     change_rate: 100,
+    //     distribution: {},
+    //     checked: false,
+    //   },
+    //   {
+    //     id: "suggestion_58",
+    //     name: "Heartburn (pyrosis)",
+    //     value: 0,
+    //     change_rate: -100,
+    //     distribution: {},
+    //     checked: false,
+    //   },
+    //   {
+    //     id: "e46a3314e9466dec61e2bbca4cf8b38bc8dbd58ca031b3a702ef3a43b82deb02",
+    //     name: "Test",
+    //     value: 0,
+    //     change_rate: -100,
+    //     distribution: {},
+    //     checked: false,
+    //   },
+    //   {
+    //     id: "5cfae3932e8ef619a14bca9f49cb1348d0be7ae17aff77117b8fcb3067191a31",
+    //     name: "吃飽太閒",
+    //     value: 0,
+    //     change_rate: -100,
+    //     distribution: {},
+    //     checked: false,
+    //   },
+    //   {
+    //     id: "19155e1e1873ef94470a7c82d83a7fcc56cd904c73234791a5fcaf720f2a79a6",
+    //     name: "Need coffee",
+    //     value: 0,
+    //     change_rate: -100,
+    //     distribution: {},
+    //     checked: false,
+    //   },{
+    //     id: "c29b18bae2991272d820eae4c3f8061bfb5495a2f5a119586f3785b2711d5055",
+    //     name: "Sleepy",
+    //     value: 0,
+    //     change_rate: -100,
+    //     distribution: {}
+    //   }, {
+    //     id: "face",
+    //     name: "Bluish lips or face",
+    //     value: 0,
+    //     change_rate: -100,
+    //     distribution: {}
+    //   }, {
+    //     id: "nasal",
+    //     name: "Nasal congestion",
+    //     value: 0,
+    //     change_rate: -100,
+    //     distribution: {}
+    //   }, {
+    //     id: "2e4a33f963deb6f28ff475891b3441b4b1582479423c655a7e36ce2350f1d442",
+    //     name: "Tired too much",
+    //     value: 0,
+    //     change_rate: -100,
+    //     distribution: {}
+    //   }, {
+    //     id: "a8db5c5202904141d3c86d9ba8e2a9950e49573d5498539829128c9beca32aa1",
+    //     name: "Loss of sense of smell",
+    //     value: 0,
+    //     change_rate: -100,
+    //     distribution: {}
+    //   }, {
+    //     id: "breath",
+    //     name: "Shortness of breath or difficulty breathing",
+    //     value: 0,
+    //     change_rate: -100,
+    //     distribution: {}
+    //   }, {
+    //     id: "fatigue",
+    //     name: "Fatigue or tiredness",
+    //     value: 0,
+    //     change_rate: -100,
+    //     distribution: {}
+    //   }, {
+    //     id: "690d323f97b4f7849c3c3f551b7f9912d99ebde5e77666de1911d56b9c5da440",
+    //     name: "火眼金星",
+    //     value: 0,
+    //     change_rate: -100,
+    //     distribution: {}
+    //   }, {
+    //     id: "chest",
+    //     name: "Chest pain",
+    //     value: 0,
+    //     change_rate: -100,
+    //     distribution: {}
+    //   }
+    // ];
   }
 
   ngOnInit() {
@@ -183,6 +183,7 @@ export class CommunityComponent implements OnInit {
       .request('get', endpoint, null, null, ApiService.DSTarget.CDS)
       .subscribe(
         (data: {report_items: any}) => {
+          this.dataBySymptoms = data.report_items;
           this.renderChart();
         },
         (err) => {
