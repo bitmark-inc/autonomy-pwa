d8cb5cd0fdc84cf25d488f4ca9cb79f2af744826
Cuong Le <cuongleqq@gmail.com>
2020-07-16 10:07:42 +0700

    Fix for the script to serve dist

--- dist-serving.js
+++ dist-serving.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 4200
+const path = require('path');
 
 app.use(express.static('./dist/autonomy'))
 app.listen(port, () => console.log(`Test app listening at http://localhost:${port}`))
-app.get("/*", function(req, res, next) {
-    res.render('dist/autonomy/index.html');
+app.get('/*', function(req, res, next) {
+    res.sendFile(path.resolve(__dirname, 'dist/autonomy/index.html'));
 });
