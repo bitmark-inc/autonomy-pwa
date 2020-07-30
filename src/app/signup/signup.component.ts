fd85e1fb8b9a9abf444e3f51198d24f780a39abc
duyenbui <duyen@bitmark.com>
2020-07-31 01:01:37 +0700

    update launch page, try to test prompt button for android on testnet

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,32 +1,41 @@
 declare var window: any;
 
-import { Component, OnInit, ChangeDetectorRef, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { UserService } from '../services/user/user.service';
 import { Router } from '@angular/router';
 
-enum EnumPageStage { Intro01, Intro02, Intro03 }
+enum EnumPageStage { Intro01, Intro02, Intro03, OnInstall }
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
   @ViewChild('addBtn') private promoptBtn: ElementRef;
 
   public PageStage = EnumPageStage;
   public stage: EnumPageStage = EnumPageStage.Intro01;
   public clickable: boolean = true;
   public deferredPrompt: any;
-  public isShowAddBtn: boolean = true;
+  public isShowAddBtn: boolean = false;
 
-  constructor(private router: Router, private userService: UserService) {}
+  // detect browser
+  public isIOSSafari: boolean;
+  public isIOSOther: boolean;
+  public isMobileExceptIOS: boolean;
+  public isDesktop: boolean;
 
+  constructor(private router: Router, private userService: UserService) {
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+      this.router.navigate(['/landing']);
+    }
+  }
+  
   ngOnInit() {
-    if (this.promoptBtn) {
-      const addBtn = this.promoptBtn.nativeElement.querySelector('.add-button');
-      console.log(addBtn);
+    this.detectBrowser();
 
+    if (this.isMobileExceptIOS) {
       window.addEventListener('beforeinstallprompt', (e) => {
         // Prevent Chrome 67 and earlier from automatically showing the prompt
         e.preventDefault();
@@ -34,58 +43,43 @@ export class SignupComponent implements OnInit {
         this.deferredPrompt = e;
         // Update UI to notify the user they can add to home screen
         this.isShowAddBtn = true;
-
-        addBtn.addEventListener('click', (e) => {
-          // hide our user interface that shows our A2HS button
-          this.isShowAddBtn = false;
-          // Show the prompt
-          this.deferredPrompt.prompt();
-          // Wait for the user to respond to the prompt
-          this.deferredPrompt.userChoice.then((choiceResult) => {
-              if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-              } else {
-                console.log('User dismissed the A2HS prompt');
-              }
-              this.deferredPrompt = null;
-            });
-        });
       });
     }
   }
 
-  // public addToHomeScreen() {
-  //   // hide our user interface that shows our A2HS button
-  //   this.isShowAddBtn = false;
-  //   // Show the prompt
-  //   this.deferredPrompt.prompt();
-  //   // Wait for the user to respond to the prompt
-  //   this.deferredPrompt.userChoice
-  //     .then((choiceResult) => {
-  //       if (choiceResult.outcome === 'accepted') {
-  //         console.log('User accepted the A2HS prompt');
-  //       } else {
-  //         console.log('User dismissed the A2HS prompt');
-  //       }
-  //       this.deferredPrompt = null;
-  //     });
-  // }
+  ngOnDestroy() {
+    if (this.isMobileExceptIOS) {
+      window.removeEventListener('beforeinstallprompt');
+    }
+  }
 
+  private detectBrowser() {
+    this.isIOSSafari = /(iPad|iPhone|iPod)/gi.test(navigator.userAgent) &&
+      !/CriOS/.test(navigator.userAgent) &&
+      !/FxiOS/.test(navigator.userAgent) &&
+      !/OPiOS/.test(navigator.userAgent) &&
+      !/mercury/.test(navigator.userAgent);
+    this.isIOSOther = /(iPad|iPhone|iPod)/gi.test(navigator.userAgent) && !this.isIOSSafari;
+    this.isMobileExceptIOS = /(Android|BlackBerry|IEMobile|Opera Mini)/gi.test(navigator.userAgent);
+    this.isDesktop = !this.isIOSSafari && !this.isIOSOther && !this.isMobileExceptIOS;
+  }
 
-  public signup(): void {
-    if (this.clickable) {
-      this.clickable = false;
-      this.userService.signup().subscribe(
-        (data) => {
-          this.clickable = true;
-          this.router.navigate(['/home']);
-        },
-        (err) => {
-          // TODO: do something
-          this.clickable = true;
-          console.log(err);
-        }
-      );
+  public addToHomeScreen() {
+    if (this.deferredPrompt) {
+      // hide our user interface that shows our A2HS button
+      this.isShowAddBtn = false;
+      // Show the prompt
+      this.deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      this.deferredPrompt.userChoice
+        .then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          this.deferredPrompt = null;
+        });
     }
   }
 
