9e4e6fd864ea36785f60bf65fb965db1a135d2a4
duyenbui <duyen@bitmark.com>
2020-07-30 16:25:09 +0700

    update landing page and try to add prompt button

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,8 @@
 declare var window: any;
 
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user/user.service'
 
 @Component({
   selector: 'app-landing',
@@ -8,14 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+  public clickable: boolean = true;
 
-  constructor() { }
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {
   }
 
   public isStandalone(): boolean {
-    return (window.matchMedia('(display-mode: standalone)').matches);
+    // return (window.matchMedia('(display-mode: standalone)').matches);
+    return true;
+  }
+
+  public signup(): void {
+    if (this.clickable) {
+      this.clickable = false;
+      this.userService.signup().subscribe(
+        (data) => {
+          this.clickable = true;
+          this.router.navigate(['/home/community']);
+        },
+        (err) => {
+          // TODO: do something
+          this.clickable = true;
+          console.log(err);
+        }
+      );
+    }
   }
 
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,6 @@
 declare var window: any;
 
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { UserService } from '../services/user/user.service';
 import { Router } from '@angular/router';
 
@@ -12,14 +12,65 @@ enum EnumPageStage { Intro01, Intro02, Intro03 }
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  @ViewChild('addBtn') private promoptBtn: ElementRef;
 
   public PageStage = EnumPageStage;
   public stage: EnumPageStage = EnumPageStage.Intro01;
   public clickable: boolean = true;
+  public deferredPrompt: any;
+  public isShowAddBtn: boolean = true;
 
   constructor(private router: Router, private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.promoptBtn) {
+      const addBtn = this.promoptBtn.nativeElement.querySelector('.add-button');
+      console.log(addBtn);
+
+      window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        this.deferredPrompt = e;
+        // Update UI to notify the user they can add to home screen
+        this.isShowAddBtn = true;
+
+        addBtn.addEventListener('click', (e) => {
+          // hide our user interface that shows our A2HS button
+          this.isShowAddBtn = false;
+          // Show the prompt
+          this.deferredPrompt.prompt();
+          // Wait for the user to respond to the prompt
+          this.deferredPrompt.userChoice.then((choiceResult) => {
+              if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+              } else {
+                console.log('User dismissed the A2HS prompt');
+              }
+              this.deferredPrompt = null;
+            });
+        });
+      });
+    }
+  }
+
+  // public addToHomeScreen() {
+  //   // hide our user interface that shows our A2HS button
+  //   this.isShowAddBtn = false;
+  //   // Show the prompt
+  //   this.deferredPrompt.prompt();
+  //   // Wait for the user to respond to the prompt
+  //   this.deferredPrompt.userChoice
+  //     .then((choiceResult) => {
+  //       if (choiceResult.outcome === 'accepted') {
+  //         console.log('User accepted the A2HS prompt');
+  //       } else {
+  //         console.log('User dismissed the A2HS prompt');
+  //       }
+  //       this.deferredPrompt = null;
+  //     });
+  // }
+
 
   public signup(): void {
     if (this.clickable) {
