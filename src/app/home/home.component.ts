1c9cf620d0fb031ac47cb4e07f1ef536795d1782
duyenbui <duyen@bitmark.com>
2020-08-05 13:40:31 +0700

    improve home tabs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,17 +5,23 @@ import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { HomepageState } from './homepage.state';
 import { Subscription } from 'rxjs';
 
+enum TabActivated { Trends, Resources, Account }
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"],
+  styleUrls: ['./home.component.scss', './setting/setting.component.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
   public isFullscreen: boolean = false;
   private isTouchDevice: boolean = 'ontouchstart' in window;
   private stateSubscription: Subscription;
-  
-  constructor(public router: Router, private ref: ChangeDetectorRef) {}
+
+  public tabActivated: TabActivated = TabActivated.Trends;
+
+  constructor(public router: Router, private ref: ChangeDetectorRef) {
+    this.setTabActivatedByUrl(this.router.url);
+  }
 
   ngOnInit() {
     this.stateSubscription = HomepageState.fullscreen.subscribe(isFullscreen => {
@@ -26,5 +32,22 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.stateSubscription.unsubscribe();
+  }
+
+  private setTabActivatedByUrl(url: string = '') {
+    switch (url) {
+    case '/home/community':
+      this.tabActivated = TabActivated.Trends;
+      break;
+    case '/home/resources':
+      this.tabActivated = TabActivated.Resources;
+      break;
+    case '/home/setting':
+      this.tabActivated = TabActivated.Account;
+      break;
+    default:
+      this.tabActivated = TabActivated.Trends;
+      break;
+    }
   }
 }
