190829395bf586b232614ee98501717789b0c148
duyenbui <duyen@bitmark.com>
2020-08-06 17:48:53 +0700

    add consent page

--- src/app/signout/signout.component.ts
+++ src/app/signout/signout.component.ts
@@ -41,7 +41,7 @@ export class SignoutComponent implements OnInit {
           rightBtn: 'try again',
           leftBtnAction: () => {
             this.bottomSheetRef.afterDismissed().subscribe(() => {
-              this.router.navigate(['/recovery-key']);
+              this.router.navigate(['/home/setting/recovery-key']);
             })
             this.bottomSheetRef.dismiss();
           },
