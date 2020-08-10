3c6bbb51da7367b971ad6f82034b531e1d6b842a
Cuong Le <cuongleqq@gmail.com>
2020-08-10 10:44:41 +0700

    Update OneSignal ID for Production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   autonomy_api_url: 'https://autonomy-api-web.bitmark.com/',
   pds_url: 'https://pds.bitmark.com/',
   cds_url: 'https://cds.bitmark.com/',
-  onesignal_app_id: 'cdb901f3-1559-43f4-a3cd-c4017f419195',
+  onesignal_app_id: '2d3c5705-6b1a-4d99-b1fa-abaefc2cdbdd',
 };
