51724079f630acd5a68925de385d1623951e64a7
Cuong Le <cuongleqq@gmail.com>
2020-08-13 14:46:56 +0700

    Remove updating lambda function from CI

--- lambda/handlers/html-template/index.js
+++ lambda/handlers/html-template/index.js
@@ -2,58 +2,85 @@
 const fs = require('fs');
 const path = require('path');
 
+const aws = require('aws-sdk');
+const s3 = new aws.S3({ region: 'us-east-1' });
+
+let indexHtmlFileContent;
+let webmanifestFileContent;
+
+s3.getObject({
+  Bucket: process.env.S3_BUCKET,
+  Key: 'index.html'
+}, function(err, data) {
+  if (err) {
+    console.log(err, err.stack); // an error occurred
+  } else {
+    console.log(data);
+    indexHtmlFileContent = data;
+  }
+});
+
+s3.getObject({
+  Bucket: process.env.S3_BUCKET,
+  Key: 'manifest.webmanifest'
+}, function(err, data) {
+  if (err) {
+    console.log(err, err.stack); // an error occurred
+  } else {
+    console.log(data);
+    webmanifestFileContent = data;
+  }
+});
+
+
 exports.handler = (event, context, callback) => {
-	const request = event.Records[0].cf.request;
+  const request = event.Records[0].cf.request;
+  const queryString = request.querystring;
 
-	let filePath = '';
+  const manifestFileRequest = /manifest.webmanifest$/;
+  const resourceRequest = /(\.[a-zA-Z0-9]{2,5}$)/;
+  
+  console.log('Query String is: ', queryString);
+  
+  let body;
 
-	if (request.uri == '/index.html' || request.uri == '/') {
-        filePath = path.join(__dirname, 'index.html');
-	} else if (request.uri == '/manifest.webmanifest') {
-		filePath = path.join(__dirname, 'manifest.webmanifest');
-	} else {
-		// do not process
-		callback(null, request);
-		return;
-	}
+  if (manifestFileRequest.test(request.uri)) {
+    body = webmanifestFileContent;
+  } else if (!resourceRequest.test(request.uri)) {
+    body = indexHtmlFileContent;
+  } else {
+    callback(null, request);
+    return;
+  }
 
-	fs.readFile(filePath, {encoding: 'utf-8'}, function(err,body){
-		if (!err) {
-			/*
-			* Generate HTTP OK response using 200 status code with HTML body.
-			*/
-			const response = {
-				status: '200',
-				statusDescription: 'OK',
-				headers: {
-					'cache-control': [{
-						key: 'Cache-Control',
-						value: 'max-age=31536000'
-					}],
-					'content-type': [{
-						key: 'Content-Type',
-						value: 'text/html'
-					}],
-					'content-encoding': [{
-						key: 'Content-Encoding',
-						value: 'UTF-8'
-					}],
-					'strict-transport-security': [{
-						key: 'Strict-Transport-Security', 
-						value: 'max-age=63072000; includeSubdomains; preload'
-					}], 
-					'x-frame-options': [{
-						key: 'X-Frame-Options', 
-						value: 'DENY'
-					}],
-				},
-				body: body,
-			};
+  const response = {
+    status: '200',
+    statusDescription: 'OK',
+    headers: {
+      'cache-control': [{
+        key: 'Cache-Control',
+        value: 'max-age=31536000'
+      }],
+      'content-type': [{
+        key: 'Content-Type',
+        value: 'text/html'
+      }],
+      'content-encoding': [{
+        key: 'Content-Encoding',
+        value: 'UTF-8'
+      }],
+      'strict-transport-security': [{
+        key: 'Strict-Transport-Security', 
+        value: 'max-age=63072000; includeSubdomains; preload'
+      }],
+      'x-frame-options': [{
+        key: 'X-Frame-Options', 
+        value: 'DENY'
+      }],
+    },
+    body: body,
+  };
 
-			//Return modified response
-			callback(null, response);
-		} else {
-			console.log(err);
-		}
-	});
+  //Return modified response
+  callback(null, response);
 };
