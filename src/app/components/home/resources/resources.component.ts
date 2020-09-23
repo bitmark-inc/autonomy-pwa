b49ca5a6537ebe98ea8a4c3b270cb9a16ccb3eae
duyenbui <duyen@bitmark.com>
2020-09-23 13:09:22 +0700

    update resources tab

--- src/app/components/home/resources/resources.component.ts
+++ src/app/components/home/resources/resources.component.ts
@@ -57,9 +57,9 @@ export class ResourcesComponent implements OnInit, OnDestroy, AfterViewInit {
   public pois: POI[] = [];
 
   // Map settings
-  private UCBekerleyLatlng: google.maps.LatLngLiteral = {
-    lat: 37.871971,
-    lng: -122.258529
+  private centerBekerleyLatlng: google.maps.LatLngLiteral = {
+    lat: 37.865136,
+    lng: -122.258432
   }
   public mapCenter: google.maps.LatLngLiteral;
   public mapIconSVGPath: string = 'M0.5 10.8975C0.5 5.09246 5.195 0.397461 11 0.397461C16.805 0.397461 21.5 5.09246 21.5 10.8975C21.5 17.1525 14.87 25.7775 12.155 29.0625C11.555 29.7825 10.46 29.7825 9.86 29.0625C7.13 25.7775 0.5 17.1525 0.5 10.8975ZM7.25 10.8975C7.25 12.9675 8.93 14.6475 11 14.6475C13.07 14.6475 14.75 12.9675 14.75 10.8975C14.75 8.82746 13.07 7.14746 11 7.14746C8.93 7.14746 7.25 8.82746 7.25 10.8975Z';
@@ -70,8 +70,8 @@ export class ResourcesComponent implements OnInit, OnDestroy, AfterViewInit {
   public mapZoomLevel: number = 18;
   public mapOptions: google.maps.MapOptions = {
     zoom: this.mapZoomLevel,
-    maxZoom: 30,
-    minZoom: 16,
+    // maxZoom: 30,
+    minZoom: 12,
     disableDefaultUI: true,
     styles: [{featureType: 'poi', stylers: [{visibility: 'off'}]}]
   };
@@ -85,7 +85,7 @@ export class ResourcesComponent implements OnInit, OnDestroy, AfterViewInit {
       this.poiType = params['poi_type'];
       this.focusedPOIID = params['focus_poi_id'];
     })
-    this.mapCenter = this.UCBekerleyLatlng;
+    this.mapCenter = this.centerBekerleyLatlng;
     this.mapHeight = `${window.innerHeight - 56 -60}px`;
     this.mapWidth = `${window.innerWidth > 768 ? 768 : window.innerWidth}px`;
     document.body.style.position = 'fixed';
